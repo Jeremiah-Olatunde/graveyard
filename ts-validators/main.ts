9169d733d65dd7c2126d7a1442da405c22f48536
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-02 00:56:51 +0100

    feat(validators typescript): add basic validators

--- ts-validators/main.ts
+++ ts-validators/main.ts
@@ -1,8 +1,39 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function validateString(value: unknown): value is string {
+	return typeof value === "string";
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+export function validateNumber(value: unknown): value is number {
+	return typeof value === "number";
+}
+
+export function validateBoolean(value: unknown): value is boolean {
+	return typeof value === "boolean";
+}
+
+export function validateArray(value: unknown): value is unknown[] {
+	return Array.isArray(value);
+}
+
+export function validateTypedArray<T>(
+	predicate: (v: unknown) => v is T,
+	value: unknown,
+): value is T[] {
+	return Array.isArray(value) && predicate(value);
+}
+
+export function validateRecord(
+	value: unknown,
+): value is Record<string, unknown> {
+	return (
+		value !== null &&
+		typeof value === "object" &&
+		Object.keys(value).every(validateString)
+	);
+}
+
+export function validateTypedRecord<T>(
+	predicate: (v: unknown) => v is T,
+	value: unknown,
+): value is Record<string, T> {
+	return validateRecord(value) && Object.values(value).every(predicate);
 }
