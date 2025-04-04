df56cb3e326e34147b9a7f558346c5038d7b594d
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-04 23:44:33 +0100

    test(validators typescript): test primitive validators

--- ts-validators/main_test.ts
+++ ts-validators/main_test.ts
@@ -1,6 +1,24 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assert, assertFalse } from "@std/assert";
+import * as validators from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function validateString() {
+	assert(validators.validateString("hello world"));
+	assertFalse(validators.validateString(10));
+	assertFalse(validators.validateString(true));
+	assertFalse(validators.validateString({ prop: "not a string" }));
+});
+
+Deno.test(function validateNumber() {
+	assert(validators.validateNumber(10));
+	assertFalse(validators.validateNumber("not a number"));
+	assertFalse(validators.validateNumber(true));
+	assertFalse(validators.validateNumber({ prop: true }));
+});
+
+Deno.test(function validateBoolean() {
+	assert(validators.validateBoolean(true));
+	assert(validators.validateBoolean(false));
+	assertFalse(validators.validateBoolean(10));
+	assertFalse(validators.validateBoolean("not a boolean"));
+	assertFalse(validators.validateBoolean({ prop: 10000 }));
 });
