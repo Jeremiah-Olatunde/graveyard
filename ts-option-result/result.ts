3df401f9cf1efbef1389215d716c01f1e45382c1
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-08 23:12:35 +0100

    feat(option-result typescript result): add map and mapErr functions

--- ts-option-result/result.ts
+++ ts-option-result/result.ts
@@ -17,3 +17,23 @@ export function err<E>(error: E): Err<E> {
 export function ok<T>(value: T): Ok<T> {
 	return { tag: "ok", value };
 }
+function map<T, U, E>(result: Result<T, E>, f: (value: T) => U): Result<U, E> {
+	switch (result.tag) {
+		case "ok":
+			return ok(f(result.value));
+		case "error":
+			return result;
+	}
+}
+
+function mapErr<T, R, E>(
+	result: Result<T, E>,
+	f: (value: E) => R,
+): Result<T, R> {
+	switch (result.tag) {
+		case "ok":
+			return result;
+		case "error":
+			return err(f(result.error));
+	}
+}
