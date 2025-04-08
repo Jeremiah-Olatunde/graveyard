f06943ae1c1fa277e82450a0c0fc656f17aa726e
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-08 23:11:57 +0100

    feat(option-result typescript result): add constructor functions

--- ts-option-result/result.ts
+++ ts-option-result/result.ts
@@ -9,3 +9,11 @@ export type Err<E> = {
 };
 
 export type Result<T, E> = Ok<T> | Err<E>;
+
+export function err<E>(error: E): Err<E> {
+	return { tag: "error", error };
+}
+
+export function ok<T>(value: T): Ok<T> {
+	return { tag: "ok", value };
+}
