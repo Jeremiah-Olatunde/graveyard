e9a7e75ee1111b55d09cf71b5409dc4cd62662c8
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-09 09:40:44 +0100

    feat(react-context-reducer react learning): add users feature

--- react-context-reducer/src/App.tsx
+++ react-context-reducer/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, useContext, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+	return (
+		<ProviderUsers>
+			<Core />
+		</ProviderUsers>
+	);
 }
 
-export default App
+function Core() {
+	const { users, addUser, deleteUser } = useContextUsers();
+	return (
+		<section>
+			<h1>Context Reducers</h1>
+			<ul>
+				{users.map((user) => {
+					return (
+						<li key={user.id}>
+							{user.id}
+							<button onClick={() => deleteUser(user)}>delete user</button>
+						</li>
+					);
+				})}
+			</ul>
+			<button onClick={() => addUser(generateUser())}>add user</button>
+		</section>
+	);
+}
+
+type User = {
+	id: string;
+};
+
+type StoreUsers = {
+	users: User[];
+	addUser: (user: User) => void;
+	deleteUser: (user: User) => void;
+};
+
+const ContextUsers = createContext<StoreUsers | null>(null);
+
+function useContextUsers() {
+	const users = useContext(ContextUsers);
+	if (users === null) throw new Error("context value not provided");
+	return users;
+}
+
+function ProviderUsers({ children }: { children: React.ReactNode }) {
+	const [users, setUsers] = useState(generateUsers());
+
+	function addUser(user: User) {
+		setUsers([...users, user]);
+	}
+
+	function deleteUser({ id }: User) {
+		setUsers(users.filter((user) => user.id !== id));
+	}
+
+	return (
+		<ContextUsers.Provider value={{ users, addUser, deleteUser }}>
+			{children}
+		</ContextUsers.Provider>
+	);
+}
+
+function generateUser(): User {
+	return { id: crypto.randomUUID() };
+}
+
+function generateUsers(): User[] {
+	const users: User[] = [];
+	for (let i = 0; i < 10; i++) users.push(generateUser());
+	return users;
+}
