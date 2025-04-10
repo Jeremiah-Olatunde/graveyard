75e37284b43ccae47a7d9b6fe92e78f673b9b778
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-10 23:18:44 +0100

    feat(struct typescript): add isSerializable type guard

--- ts-struct/main.ts
+++ ts-struct/main.ts
@@ -4,3 +4,23 @@ export type Serializable =
 	| boolean
 	| Serializable[]
 	| { [index: string]: Serializable };
+
+function isSerializable(value: unknown): value is Serializable {
+	if (
+		typeof value === "string" ||
+		typeof value === "number" ||
+		typeof value === "boolean"
+	) {
+		return true;
+	}
+
+	if (Array.isArray(value)) {
+		return value.every(isSerializable);
+	}
+
+	if (typeof value === "object" && value !== null) {
+		return Object.values(value).every(isSerializable);
+	}
+
+	return false;
+}
