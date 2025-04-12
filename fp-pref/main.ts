1261149d217f20f9130c5963c953a92d76d3e754
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-12 23:47:29 +0100

    feat(fp-perf typescript): add filter function

--- fp-pref/main.ts
+++ fp-pref/main.ts
@@ -4,6 +4,26 @@ export function map<Item, Mapped>(
 ): readonly Mapped[] {
 	const [head, ...tail] = array;
 
-	if (head === undefined) return [];
+	if (head === undefined) {
+		return [];
+	}
+
 	return [mapping(head), ...map(tail, mapping)];
+}
+
+export function filter<Item>(
+	array: readonly Item[],
+	predicate: (item: Item) => boolean,
+): Item[] {
+	const [head, ...tail] = array;
+
+	if (head === undefined) {
+		return [];
+	}
+
+	if (predicate(head)) {
+		return [head, ...filter(tail, predicate)];
+	}
+
+	return filter(tail, predicate);
 }
