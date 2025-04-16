06d5b1a617526e9753258d17756d1f3b1e41f3ab
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-16 23:59:56 +0100

    feat(ts-belt typescript): testing option contains function

--- ts-belt/src/index.ts
+++ ts-belt/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello world");
+import { F, O } from "@mobily/ts-belt";
+import assert from "node:assert";
+
+{
+	const ten = O.Some(10);
+	assert.equal(true, O.contains(10, ten));
+	assert.equal(true, O.contains(10)(ten));
+}
