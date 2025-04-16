68ff9ea4abc717ca8ac72a17ec300103c117a2cd
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-04-17 00:24:44 +0100

    feat(ts-belt typescript): test option fromExecution

--- ts-belt/src/index.ts
+++ ts-belt/src/index.ts
@@ -1,8 +1,36 @@
-import { F, O } from "@mobily/ts-belt";
+import { A, F, O } from "@mobily/ts-belt";
 import assert from "node:assert";
 
 {
 	const ten = O.Some(10);
 	assert.equal(true, O.contains(10, ten));
 	assert.equal(true, O.contains(10)(ten));
+}
+
+{
+	const some = O.fromExecution(
+		() => "turning and turning in the widening gyre",
+	);
+
+	assert(O.isSome(some));
+
+	const none = O.fromExecution(() => {
+		throw "error";
+	});
+
+	assert(O.isNone(none));
+}
+
+{
+	const uncertain = () => {
+		const boom = Math.random() < 0.5;
+
+		if (boom) {
+			throw "BOOM!!";
+		}
+
+		return Math.floor(Math.random() * 10);
+	};
+
+	const testArray = A.makeWithIndex(10, () => O.fromExecution(uncertain));
 }
