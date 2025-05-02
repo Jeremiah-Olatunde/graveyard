427de529da7b024e0a17f2c9c53e75e0ff6d891e
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-05-02 22:20:16 +0100

    feat(typescript comma-delimited-string): refactor

--- ts_comma_delimited_string/main.ts
+++ ts_comma_delimited_string/main.ts
@@ -1,47 +1,27 @@
-export function commaDelimitedAmt(val: string): string {
-	const [whole, fractional = ""] = val.split(".");
-	const comma = fn(whole).join(",");
-	return `${comma}.${fractional.padEnd(2, "0")}`;
+export function take(xs: string, count: number): string {
+	return xs.slice(0, count);
 }
 
-function group(xs: string, count: number): string[] {
+export function drop(xs: string, count: number): string {
+	return xs.slice(count);
+}
+
+export function group(xs: string, count: number): string[] {
 	if (xs === "") return [];
-	const [batched, tail] = splitN(xs, count);
-	return [batched, ...group(tail, count)];
+	const head = take(xs, count);
+	const tail = drop(xs, count);
+	return [head, ...group(tail, count)];
 }
 
-function groupIterative(xs: string, count: number): string[] {
-	const grouped: string[] = [];
-
-	for (let i = 0; i < xs.length; i += 3) {
-		grouped.push(xs.slice(i, i + 3));
-	}
-
-	return grouped;
-}
-
-function splitN(xs: string, count: number): [string, string] {
-	return [xs.slice(0, count), xs.slice(count)];
-}
-
-function fn(xs: string): string[] {
+function group3(xs: string): string[] {
 	const mod = xs.length % 3;
-	if (mod === 0) return groupIterative(xs, 3);
-
-	const [first, tail] = splitN(xs, mod);
-	return [first, ...groupIterative(tail, 3)];
+	const head = take(xs, mod);
+	const tail = drop(xs, mod);
+	return [head, ...group(tail, 3)];
 }
 
-function random(): string {
-	return Math.floor(Math.random() * 1_000_000).toString();
+export function commaDelimitedAmount(amount: string): string {
+	const [whole, fractional = ""] = amount.split(".");
+	const formattedWhole = group3(whole);
+	return `${formattedWhole}.${fractional.padEnd(2, "0")}`;
 }
-
-console.log(commaDelimitedAmt(random()));
-console.log(commaDelimitedAmt(random()));
-console.log(commaDelimitedAmt(random()));
-console.log(commaDelimitedAmt(random()));
-console.log(commaDelimitedAmt(random()));
-console.log(commaDelimitedAmt(random()));
-console.log(commaDelimitedAmt(random()));
-
-console.log("hello world".slice(0, 3));
