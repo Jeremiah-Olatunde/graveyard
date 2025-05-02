e7eb685014bc7746b8b2d7e2ce25aad7e86ffce2
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-05-02 21:46:32 +0100

    feat(typescript comma-delimited-string): add commaDelimiteAmnt function

--- ts_comma_delimited_string/main.ts
+++ ts_comma_delimited_string/main.ts
@@ -1,8 +1,47 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function commaDelimitedAmt(val: string): string {
+	const [whole, fractional = ""] = val.split(".");
+	const comma = fn(whole).join(",");
+	return `${comma}.${fractional.padEnd(2, "0")}`;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+function group(xs: string, count: number): string[] {
+	if (xs === "") return [];
+	const [batched, tail] = splitN(xs, count);
+	return [batched, ...group(tail, count)];
 }
+
+function groupIterative(xs: string, count: number): string[] {
+	const grouped: string[] = [];
+
+	for (let i = 0; i < xs.length; i += 3) {
+		grouped.push(xs.slice(i, i + 3));
+	}
+
+	return grouped;
+}
+
+function splitN(xs: string, count: number): [string, string] {
+	return [xs.slice(0, count), xs.slice(count)];
+}
+
+function fn(xs: string): string[] {
+	const mod = xs.length % 3;
+	if (mod === 0) return groupIterative(xs, 3);
+
+	const [first, tail] = splitN(xs, mod);
+	return [first, ...groupIterative(tail, 3)];
+}
+
+function random(): string {
+	return Math.floor(Math.random() * 1_000_000).toString();
+}
+
+console.log(commaDelimitedAmt(random()));
+console.log(commaDelimitedAmt(random()));
+console.log(commaDelimitedAmt(random()));
+console.log(commaDelimitedAmt(random()));
+console.log(commaDelimitedAmt(random()));
+console.log(commaDelimitedAmt(random()));
+console.log(commaDelimitedAmt(random()));
+
+console.log("hello world".slice(0, 3));
