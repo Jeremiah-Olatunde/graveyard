59443fb0660e0622d01f96040961796128086384
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-05-04 14:29:01 +0100

    feat(react rhf_validation): add password field with custom validation

--- react_form_validation/src/App.tsx
+++ react_form_validation/src/App.tsx
@@ -1,16 +1,20 @@
 import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
 
 type FormData = {
-	name: string;
+	password: string;
 };
 
 export default function App() {
-	const { register, handleSubmit, watch } = useForm<FormData>();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>({
+		defaultValues: { password: "" },
+	});
 	const onSuccess: SubmitHandler<FormData> = (data) => console.log(data);
 	const onError: SubmitErrorHandler<FormData> = (errors) =>
 		console.error(errors);
-
-	console.log(watch("name"));
 
 	return (
 		<>
@@ -21,9 +25,32 @@ export default function App() {
 
 				<form onSubmit={handleSubmit(onSuccess, onError)}>
 					<div>
-						<label htmlFor="name">name</label>
+						<label htmlFor="password">password</label>
 						<br />
-						<input {...register("name", { required: true })} />
+						<input
+							{...register("password", {
+								required: { value: true, message: "password is required" },
+								validate: {
+									hasValidLength: (value) => {
+										return 8 < value.length || "length must be < 8 characters";
+									},
+									hasUppercase: (value) => {
+										return /[A-Z]/.test(value) || "must have a capital letter";
+									},
+									hasLowercase: (value) => {
+										return /[a-z]/.test(value) || "must have a capital letter";
+									},
+									hasSymbol: (value) => {
+										return /[@#$%^&*!()]/.test(value) || "must have a symbol";
+									},
+									hasDigit: (value) => {
+										return /[0-9]/.test(value) || "must contain digit";
+									},
+								},
+							})}
+						/>
+
+						{errors.password && <div>{errors.password.message}</div>}
 					</div>
 
 					<input type="submit" value="submit" />
